07f945c2cfa14afe7b0ef06d0424aeb040fccba6
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-27 11:43:04 +0530

    Binary Serch Generic

--- BinarySerchGeneric.java
+++ BinarySerchGeneric.java
@@ -3,7 +3,7 @@ package Algorithm;
 public class BinarySerchGeneric<T> {
 	
 
-	public <T extends Comparable<? super T>> int search(T[] list, int first, int last, T key) {
+	public <T extends Comparable<T>>  int search(T[] list, int first, int last, T key) {
 		int foundPosition;
 		int mid = first + (last - first) / 2;
 		if (first > last)
@@ -18,31 +18,19 @@ public class BinarySerchGeneric<T> {
 	}
 
 	public static void main(String args[]) {
-		BinarySerchGeneric n = new BinarySerchGeneric();
+		BinarySerchGeneric<Integer> n = new BinarySerchGeneric<>();
 	    //Integer
 	    Integer [] searchInteger = {0,2,4,6,8,10,12,14,16};
-	   
-	    System.out.println("Integer test array contains...");
-	        for (Integer a1 : searchInteger) {
-	         System.out.print(a1 + " ");
-	        }
-		// String
-		String[] searchFruits = { "lemon", "apple", "banana", "peach", "pineapple", "grapes", "blueberry", "papaya" };
-		System.out.println("String test array contains...");
-		for (String a1 : searchFruits) {
-			System.out.print(a1 + " ");
-		}
-		System.out.println("\nChecking String array...");
-		int results;
-		int fruitLast = searchFruits.length - 1;
-		for (int key = 0; key < searchFruits.length; key++) {
-			results = n.search(searchFruits, 0, fruitLast, searchFruits[key]);
-			System.out.println("Key = " + searchFruits[key]);
-			System.out.println("Index result = " + results);
-			if (results < 0)
-				System.out.println(searchFruits[key] + " is not in the array.");
-			else
-				System.out.println(searchFruits[key] + " is at index " + results + ".");
-		}
+	
+	        System.out.println("\nChecking Integer array...");
+			int results;
+			int lengthOfArray = searchInteger.length - 1;
+			for (int key = 0; key < searchInteger.length; key++) {
+				results = n.search(searchInteger, 0, lengthOfArray, searchInteger[key]);
+				if (results < 0)
+					System.out.println("\n"+searchInteger[key] + " is not in the array.");
+				else
+					System.out.println("\n"+searchInteger[key] + " is at index " + results);
+			}
 	}
 }
