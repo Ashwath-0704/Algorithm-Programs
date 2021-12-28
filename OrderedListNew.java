2b61aedad795342c397df1506a3138796e686151
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-28 18:28:22 +0530

    Algorithm Programs

--- OrderedListNew.java
+++ OrderedListNew.java
@@ -1,13 +1,11 @@
 package Algorithm;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Scanner;
-
+s
 public class OrderedListNew {
 	static Scanner sc = new Scanner(System.in);
-
 	@SuppressWarnings("static-access")
 	public static void orderedList() throws IOException {
 		BinarySearcherNew bs = new BinarySearcherNew();
@@ -16,31 +14,20 @@ public class OrderedListNew {
 			System.out.println(bs.list1);
 			System.out.println("Enter the word to search");
 			String Searchword = sc.next();
-			// Adding word
-			if (!bs.list1.contains(Searchword)) {
-				bs.list1.add(Searchword);
-			}
-			// Removing word
-			else {
-				bs.list1.remove(Searchword);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+			if (!bs.list1.contains(Searchword)) {bs.list1.add(Searchword);}	// Adding word
+			else {bs.list1.remove(Searchword);}	// Removing word
+		} 
+		catch (FileNotFoundException e) {e.printStackTrace();}
 		Collections.sort(bs.list1);
 		System.out.println(bs.list1);
 	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
 			orderedList();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			} 
+		catch (IOException e) {
 			e.printStackTrace();
-		}
+			}
 	}
-
 }
-
-//C:\\Users\\hp\\OneDrive\\BridgeLabz\\eclipse-workspace\\AlgorithmPrograms\\src\\Algorithm\\input.txt
+//C:\\Users\\hp\\OneDrive\\BridgeLabz\\eclipse-workspace\\AlgorithmPrograms\\src\\Algorithm\\input.txts
--- UnorderedListNew.java
+++ UnorderedListNew.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class UnorderedListNew {
 	static Scanner sc = new Scanner(System.in);
-
 	@SuppressWarnings("static-access")
 	public static void unOrderedList() throws IOException {
 		BinarySearcherNew bs = new BinarySearcherNew();
@@ -15,24 +14,16 @@ public class UnorderedListNew {
 			System.out.println(bs.list1);
 			System.out.println("Enter the word to search");
 			String Searchword = sc.next();
-			// Adding word
-			if (!bs.list1.contains(Searchword)) {
-				bs.list1.add(Searchword);
-			}
-			// Removing word
-			else {
-				bs.list1.remove(Searchword);
-			}
+			
+			if (!bs.list1.contains(Searchword)) { bs.list1.add(Searchword); }// Adding word
+			else { bs.list1.remove(Searchword);}// Removing word
+			
 			System.out.println(bs.list1);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}
+		catch (FileNotFoundException e) {e.printStackTrace();}
 	}
-
 	public static void main(String[] args) throws IOException {
 		unOrderedList();
 	}
-
 }
-
 //C:\\Users\\hp\\OneDrive\\BridgeLabz\\eclipse-workspace\\AlgorithmPrograms\\src\\Algorithm\\input.txt
